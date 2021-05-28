1bf2a28573f881258ad32165e5e5691b386216d9
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2021-05-28 18:53:36 +0200

    testPasswordNull, testPasswordUpperLowerCase

--- src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
@@ -7,20 +7,48 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordValidatorTest {
 
     @Test
-    @DisplayName("Has password valid length")
+    @DisplayName("Password is NULL")
+    public void testPasswordNull(){
+        PasswordValidator pw = new PasswordValidator();
+        assertThrows(IllegalArgumentException.class, ()->pw.checkPassword(null));
+    }
+
+    @Test
+    @DisplayName("Password length valid")
     public void testPasswordLength1(){
         PasswordValidator pw = new PasswordValidator();
         assertTrue(pw.checkPassword("password1234"));
     }
     @Test
+    @DisplayName("Password length too short")
     public void testPasswordLength2(){
         PasswordValidator pw = new PasswordValidator();
         assertFalse(pw.checkPassword("pw1"));
     }
     @Test
+    @DisplayName("Password length too long")
     public void testPasswordLength3(){
         PasswordValidator pw = new PasswordValidator();
         assertFalse(pw.checkPassword("pw1234pw1234pw1234pw1234pw"));
+    }
+
+    @Test
+    @DisplayName("password has upper and lower case letters")
+    public void testPasswordUpperLowerCase1(){
+        PasswordValidator pw = new PasswordValidator();
+        assertTrue(pw.checkPassword("Abcdefgh"));
+    }
+    @Test
+    @DisplayName("password has no upper case letter")
+    public void testPasswordUpperLowerCase2(){
+        PasswordValidator pw = new PasswordValidator();
+        assertFalse(pw.checkPassword("abcdefgh"));
+    }
+    @Test
+    @DisplayName("password has no lower case letter")
+    public void testPasswordUpperLowerCase3(){
+        PasswordValidator pw = new PasswordValidator();
+        assertFalse(pw.checkPassword("ABCDEFGH"));
     }
 
 }
